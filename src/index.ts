f9624f8cb09882099997ea3d5a0c05f183b54f13
Gabriel Olaru <gabriel.olaru@bolt.eu>
2022-06-23 17:54:42 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * This is a test function for jsdoc
+ */
+const test = () => {
+    console.log("This is a test!");
+}
+
+/**
+ * Retrieves a user by email.
+ * @async
+ * @method
+ * @param {number} id - User email
+ * @returns {string} User object
+ */
+const getUserByID = (id: number): string => {
+    return "Test";
+}
+
+/**
+ * This is a beatiful class
+ */
+class Joke {
+    constructor() {
+
+    }
+}
